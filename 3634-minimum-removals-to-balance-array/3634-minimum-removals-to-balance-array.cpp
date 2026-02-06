99f6023a31ea81bfd9d6b5c3dc3f7bebd4b44d4a
Somesh Raj <sombabaofficial@gmail.com>
2026-02-06 09:48:35 +0530

    Time: 35 ms (48.84%), Space: 104.9 MB (98.84%) - LeetHub

--- 3634-minimum-removals-to-balance-array/3634-minimum-removals-to-balance-array.cpp
+++ 3634-minimum-removals-to-balance-array/3634-minimum-removals-to-balance-array.cpp
@@ -3,7 +3,7 @@ public:
     int minRemoval(vector<int>& nums, int k) {
         int n = nums.size();
         int j=0;
-        int ans=1;
+        int ans=0;
         sort(nums.begin(),nums.end());
         for(int i=0;i<n;i++)
         {
