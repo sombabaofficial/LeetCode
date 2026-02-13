ba8dfa041ab912e99d7903f79ac48fdcbec552c0
Somesh Raj <sombabaofficial@gmail.com>
2026-02-13 17:23:39 +0530

    Time: 4 ms (33.02%), Space: 14.8 MB (97.22%) - LeetHub

--- 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.cpp
+++ 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.cpp
@@ -1,39 +1,21 @@
+
 class Solution {
 public:
-    typedef pair<int,int> pi;
-    struct cmp {
-        bool operator()(pi a, pi b)
-        {
-            if(a.first == b.first)
-                return a.second < b.second;  
-            return a.first > b.first;        
-        }
-    };
+    static unordered_map<int,int>mpp;
+    static bool cmp(int a , int b)
+    {
+        if(mpp[a]==mpp[b]) return a>b;
+        return mpp[a]<mpp[b];
+    }
     vector<int> frequencySort(vector<int>& nums) {
-
-        unordered_map<int,int>mpp;
-
+  
+        mpp.clear();
         for(auto it:nums)mpp[it]++;
 
-        priority_queue<pi,vector<pi>,cmp>pq;
+        sort(nums.begin(),nums.end(),cmp);
 
-        for(auto it : mpp)
-        {
-            pq.push({it.second,it.first});
-        }
-
-        vector<int>ans;
-
-        while(!pq.empty())
-        {
-            int freq = pq.top().first;
-            int num = pq.top().second;
-            pq.pop();
-
-            for(int i=0;i<freq;i++) ans.push_back(num);
-        }
-
-        return ans;
+        return nums;
         
     }
 };
+unordered_map<int,int> Solution::mpp;
