52b57f1c6b41cd3b03bd3a8a84e825d97c7abb33
Somesh Raj <sombabaofficial@gmail.com>
2026-07-11 06:43:41 +0530

    Time: 27 ms (88.3%), Space: 129.4 MB (64.13%) - LeetHub

---
+++ 2685-count-the-number-of-complete-components/2685-count-the-number-of-complete-components.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    vector<vector<int>> graph;
+    vector<bool> visited;
+    vector<int> component;
+    void dfs(int n){
+        if(visited[n]) return;
+        component.push_back(n);
+        visited[n] = true;
+        for(auto w: graph[n]) if(!visited[w]){
+            dfs(w);
+        }
+    }
+    int countCompleteComponents(int n, vector<vector<int>>& edges) {
+        graph.resize(n); visited.resize(n);
+   
+        for(auto& e: edges){
+            graph[e[0]].push_back(e[1]);
+            graph[e[1]].push_back(e[0]);
+        }
+
+        int ans = 0;
+        for(int i = 0; i < n; i++) if(!visited[i]){
+            dfs(i); 
+            int aux = component.size() - 1;
+            bool done = true;
+            for(auto& c: component){
+                if(graph[c].size() != aux) {
+                    done = false; break;
+                }
+            }
+            if(done)ans++;
+            component.erase(component.begin(), component.end());
+        }
+
+        return ans;
+    }
+};
